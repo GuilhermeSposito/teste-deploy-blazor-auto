338a8f4585da52d9cdc6d408511d122904f2eea0
Guilherme Sposito <guilhermesposito14@gmail.com>
2025-05-30 10:18:36 -0300

    Teste

--- MudBlazorWebApp3/MudBlazorWebApp3.Client/Program.cs
+++ MudBlazorWebApp3/MudBlazorWebApp3.Client/Program.cs
@@ -3,6 +3,10 @@ using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri("https://syslogicadev.com/api/v1/")
+});
 
 builder.Services.AddMudServices();
 
